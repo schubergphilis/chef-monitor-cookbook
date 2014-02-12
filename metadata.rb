672217f98181aec3ef5f4070cdb23ceccdf39b8d
Sander Botman <sbotman@schubergphilis.com>
2014-02-13 00:58:28 +0100

    adding the metadata for the cookbook

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'chef_monitor'
+maintainer       'Sander Botman'
+maintainer_email 'sbotman@schubergphilis.com'
+license          'Apache License, Version 2.0'
+description      'Installs/Configures chef-logmon and chef-worker'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
